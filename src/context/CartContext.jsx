74c0b95180863c883d1f4e8814ff9d19c643f3a5
rajagokilab <rajagokilab@gmail.com>
2025-11-08 20:50:44 +0100

    URL1

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -3,7 +3,7 @@ import { getAuthToken } from '../components/auth'; // Adjust path if needed
 
 // --- Constants ---
 const GUEST_CART_ID_KEY = 'guestCartId';
-const API_BASE = 'http://127.0.0.1:8000/api/cart';
+const API_BASE = `${import.meta.env.VITE_API_URL}/cart`;
 
 const CartContext = createContext();
 
